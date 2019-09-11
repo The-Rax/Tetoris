dd8de4883abb97ee4c8862394e4bfa52b63c9b78
The-Rax <e_kingdomhearts@docomo.ne.jp>
2019-09-12 08:20:21 +0900

    1st commit

--- tetoris.c
+++ tetoris.c
@@ -37,31 +37,31 @@ char minoS[MINO_T_MAX][MINO_A_MAX][MINO_H][MINO_W] = {   //ミノ
   {
     //MINO_A_0
     {
-      0,1,0,0,
-      0,1,0,0,
-      0,1,0,0,
-      0,1,0,0,
+      0,0,0,0,
+      0,0,0,0,
+      1,1,1,1,
+      0,0,0,0,
     },
     //MINO_A_1
     {
-      0,0,0,0,
-      0,0,0,0,
-      1,1,1,1,
-      0,0,0,0,
+      0,0,1,0,
+      0,0,1,0,
+      0,0,1,0,
+      0,0,1,0,
     },
     //MINO_A_2
     {
-      0,0,1,0,
-      0,0,1,0,
-      0,0,1,0,
-      0,0,1,0,
-    },
-    //MINO_A_3
-    {
       0,0,0,0,
       1,1,1,1,
       0,0,0,0,
       0,0,0,0,
+    },
+    //MINO_A_3
+    {
+      0,1,0,0,
+      0,1,0,0,
+      0,1,0,0,
+      0,1,0,0,
     },
   },
   //MINO_T_O
@@ -161,61 +161,61 @@ char minoS[MINO_T_MAX][MINO_A_MAX][MINO_H][MINO_W] = {   //ミノ
   {
     //MINO_A_0
     {
+      0,0,0,0,
+      0,1,0,0,
+      0,1,1,1,
+      0,0,0,0,
+    },
+    //MINO_A_1
+    {
       0,0,1,0,
       0,0,1,0,
       0,1,1,0,
       0,0,0,0,
     },
-    //MINO_A_1
+    //MINO_A_2
     {
       0,0,0,0,
       1,1,1,0,
       0,0,1,0,
       0,0,0,0,
     },
-    //MINO_A_2
+    //MINO_A_3
     {
       0,0,0,0,
       0,1,1,0,
       0,1,0,0,
       0,1,0,0,
-    },
-    //MINO_A_3
-    {
-      0,0,0,0,
-      0,1,0,0,
-      0,1,1,1,
-      0,0,0,0,
     },
   },
   //MINO_T_L
   {
     //MINO_A_0
     {
-      0,1,0,0,
-      0,1,0,0,
-      0,1,1,0,
-      0,0,0,0,
-    },
-    //MINO_A_1
-  {
       0,0,0,0,
       0,0,1,0,
       1,1,1,0,
       0,0,0,0,
     },
-    //MINO_A_2
+    //MINO_A_1
     {
       0,0,0,0,
       0,1,1,0,
       0,0,1,0,
       0,0,1,0,
     },
-    //MINO_A_3
+    //MINO_A_2
     {
       0,0,0,0,
       0,1,1,1,
       0,1,0,0,
+      0,0,0,0,
+    },
+    //MINO_A_3
+    {
+      0,1,0,0,
+      0,1,0,0,
+      0,1,1,0,
       0,0,0,0,
     },
   },
@@ -224,53 +224,79 @@ char minoS[MINO_T_MAX][MINO_A_MAX][MINO_H][MINO_W] = {   //ミノ
     //MINO_A_0
     {
       0,0,0,0,
-      1,1,1,0,
-      0,1,0,0,
-      0,0,0,0,
-    },
-    //MINO_A_1
-    {
-      0,0,0,0,
-      0,1,0,0,
-      0,1,1,0,
-      0,1,0,0,
-    },
-    //MINO_A_2
-    {
-      0,0,0,0,
       0,0,1,0,
       0,1,1,1,
       0,0,0,0,
     },
-    //MINO_A_3
+    //MINO_A_1
     {
       0,0,1,0,
       0,1,1,0,
       0,0,1,0,
       0,0,0,0,
     },
+    //MINO_A_2
+    {
+      0,0,0,0,
+      1,1,1,0,
+      0,1,0,0,
+      0,0,0,0,
+    },
+    //MINO_A_3
+    {
+      0,0,0,0,
+      0,1,0,0,
+      0,1,1,0,
+      0,1,0,0,
+    },
   }
 };
 
-int minoT = 0, minoA = 0;
-int minoX = 5, minoY = 0;
+int next [5] = {};  //この後のミノの種類
+int now;            //ミノ更新用の仮場所
+int stock;          //ストック用
+int isstock = 0;    //ストック中か判定
+int canstock = 1;   //ストック可能か判定
+
+int minoX = 5, minoY = -1;  //操作対象のミノの現在地
+int minoT = 0, minoA = 0;  //操作対象のミノの種類と角度
 
 void display() {  //表示用関数
  memcpy(displaybuf, field, sizeof(field));  
 
-      for (int i = 0; i < MINO_H; i ++)  //ミノによる画面更新
-        for (int j = 0; j < MINO_W; j ++)
-        displaybuf[minoY + i][minoX + j] |= minoS[minoT][minoA][i][j];
+  for (int i = 0; i < MINO_H; i ++)  //ミノによる画面更新
+    for (int j = 0; j < MINO_W; j ++)
+      displaybuf[minoY + i][minoX + j] |= minoS[minoT][minoA][i][j];
       
       
-      system("cls");  //画面リセット
+  system("cls");  //画面リセット
       
-      for (int i = 0; i < FIELD_H; i++){  
-        for (int j = 0; j < FIELD_W; j++)
-          printf(displaybuf[i][j] ? "■" : "　");
-        printf("\n");
-        }
+  for (int i = 0; i < FIELD_H; i++){
+    if (i == 0){                       //ストック画面表示
+      printf("ｓｔｏｃｋ");
+    }else if (i < 5 && isstock == 1){
+      for(int j = 0; j < 4; j++){
+        printf(minoS[stock][0][i - 1][j] ? "■" : "　");
+      }
 
+      printf("　");
+    }else{
+      printf("　　　　　");
+    }
+
+    for (int j = 0; j < FIELD_W; j++){ //フィールド表示
+      printf(displaybuf[i][j] ? "■" : "　");
+    }
+
+    if(i < 21){                        //この後のミノ表示
+      printf("　");
+      for (int j = 0; j < MINO_W; j++){
+        printf(minoS[next[i / MINO_H]][0][i % MINO_H][j] ? "■" : "　");
+      }
+    }
+
+    printf("\n");
+  }
 }
 
 bool ishit(int _minoX, int _minoY, int _minoT, int _minoA) {  //あたり判定
@@ -284,9 +310,9 @@ bool ishit(int _minoX, int _minoY, int _minoT, int _minoA) {  //あたり判定
 }
 
 void newmino(){  //ミノ生成
-  minoX = 5; minoY = 0;
+  minoX = 5; minoY = -1;
   minoT = rand() % MINO_T_MAX;
-  minoA = rand() % MINO_A_MAX;
+  minoA = 0;
 }
 void main (){
   memset (field, 0, sizeof(field));  //フィールド初期化
@@ -296,32 +322,71 @@ void main (){
   for (int i = 0; i < FIELD_W; i ++)
     field[FIELD_H - 1][i] = 1; 
 
+  
+  for (int i = 0; i < 5; i++){       //この後のミノ生成
   newmino();
+  next[i] = minoT;
+  }
 
-  time_t t = time(NULL);  
-  while (1) {
-    if(_kbhit()) {  //標準入力時の処理
+  newmino();                        //操作対象のミノ生成
+
+  time_t t = time(NULL);            //時間管理用変数
+
+  while (1) {                       //終了判定
+    if(ishit(minoX, minoY, minoT, minoA)){
+      printf("game over\n");
+      break;
+    }
+
+    if(_kbhit()) {                 //標準入力時の処理
       switch (_getch()){
-        case 'w' : while (!ishit(minoX, minoY + 1, minoT, minoA)){minoY++;}; break;
-        case 's' : if (!ishit(minoX, minoY + 1, minoT, minoA)){minoY++;}; break;
-        case 'a' : if (!ishit(minoX - 1, minoY, minoT, minoA)){minoX--;}; break;
-        case 'd' : if (!ishit(minoX + 1, minoY, minoT, minoA)){minoX++;}; break;
-        case 'k' : if (!ishit(minoX, minoY, minoT, (minoA + 1)%MINO_A_MAX)){minoA = (minoA + 1) % MINO_A_MAX;};
-        case 'l' : if (!ishit(minoX, minoY, minoT, (minoA - 1)%MINO_A_MAX)){minoA = (minoA - 1) % MINO_A_MAX;};
+        case 'w' : while (!ishit(minoX, minoY + 1, minoT, minoA)){minoY++;}; break;  //上
+        case 's' : if (!ishit(minoX, minoY + 1, minoT, minoA)){minoY++;}; break;     //下
+        case 'a' : if (!ishit(minoX - 1, minoY, minoT, minoA)){minoX--;}; break;     //左
+        case 'd' : if (!ishit(minoX + 1, minoY, minoT, minoA)){minoX++;}; break;     //右
+        case 'k' :                                                                   //左回転
+        if (!ishit(minoX, minoY, minoT, (minoA + 1)%MINO_A_MAX)){minoA = (minoA + 1) % MINO_A_MAX;}; 
+        break;
+        case 'l' :                                                                   //右回転
+        if (!ishit(minoX, minoY, minoT, (minoA + 3)%MINO_A_MAX)){minoA = (minoA + 3) % MINO_A_MAX;}; 
+        break;
+        case 'h' :                                                                  //ストック
+        if (canstock){        //ストック可能か判定
+          if (isstock){       //ストック中か判定
+            now = stock;
+            stock = minoT;
+            newmino();
+            minoT = now;
+          }else{
+            stock = minoT;    //ストックを今のミノで更新
+
+            now = next[0];    //この後のミノ更新
+            for(int i = 0; i < 4; i++){
+              next[i] = next[i + 1];
+            }
+            newmino();
+            next[4] = minoT;
+            minoT = now;
+
+            isstock = 1;     //ストック中判定に更新
+          }
+          canstock = 0;      //1一度ストックしたのでストック不可に更新
+        }
       }
+
       display();
     };
     if (t != time(NULL)){  //1秒経過
-      t = time(NULL);
+      t = time(NULL);  //時間の基準点更新
 
-      if (ishit(minoX, minoY + 1, minoT, minoA)){
+      if (ishit(minoX, minoY + 1, minoT, minoA)){  //ミノが地面に接触したとき
         for (int i = 0; i < MINO_H; i ++){
           for (int j = 0; j < MINO_W; j ++){
             field[minoY + i][minoX + j] |= minoS[minoT][minoA][i][j];  //フィールドをミノで更新
           }
         };
 
-        for(int i = 0 ; i < FIELD_H - 1; i++){  
+        for (int i = 0 ; i < FIELD_H - 1; i++){  
           bool line = true;
           for (int j = 1;  j < FIELD_W - 1; j++){
             if (!field[i][j])  //横幅全部詰まっているか判定
@@ -332,16 +397,25 @@ void main (){
             field[i][j] = 0;
              */
 
-            for(int j = i; 0<= j; j--)
+            for(int j = i; 0 <= j; j--)
             memcpy(field[j], field[j - 1], FIELD_W);  //上のマスをコピー
           }
         }
-        
+
+        now = next[0];                //ミノ更新
+        for(int i = 0; i < 4; i++){
+          next[i] = next[i + 1];
+        }
         newmino();
+        next[4] = minoT;
+        minoT = now;
+        
+        canstock = 1;
       }
       else{
         minoY++;
-        };
+      };
+
       display();
     } 
   }
